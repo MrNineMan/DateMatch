4e5765110081057ddbc2bd9b695f342bb3ac9a3c
MrNineMan <bearth2501@yahoo.com>
2023-05-06 10:33:08 +0200

    A simple project that showcases the capabilities of AtomicJar's Testcontainer's library

---
+++ src/main/java/org/draft/dev/BirthCheck.java
@@ -0,0 +1,22 @@
+package org.draft.dev;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class BirthCheck {
+
+
+    void removeRecords(ResultSet rs) throws SQLException {
+        while (rs.next()) {
+            java.sql.Date birthDate = rs.getDate(2);             // Retrieve only the first column value
+            if (1990 > birthDate.getYear()) {
+                rs.deleteRow();
+            }
+
+        }
+    }
+
+}
+
+
---
+++ src/test/java/org/draft/dev/BirthCheckTest.java
@@ -0,0 +1,49 @@
+package org.draft.dev;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.Test;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Testcontainers
+public class BirthCheckTest {
+
+    @Container
+    public static MySQLContainer<?> sqlContainer = new MySQLContainer<>("mysql:5.5");
+    private ResultSet rs;
+
+
+    @BeforeEach
+    public void setUp() {
+        try {
+            Connection conn = DriverManager.getConnection(sqlContainer.getJdbcUrl(), sqlContainer.getUsername(), sqlContainer.getPassword());
+            Statement state = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+            state.execute("CREATE TABLE Persons (\n" +  //You can return a
+                    "    ID INT NOT NULL DEFAULT 0, \n" +
+                    "    BirthDate varchar(255),\n" +
+                    "    FirstName varchar(255),\n" +
+                    "    LastName varchar(255), \n" +
+                    "    PRIMARY KEY(ID));");
+
+            state.execute("INSERT INTO Persons (ID, Birthdate, FirstName, LastName) \n" +
+                    "values (1,'safdasf', 'John', 'Smith'), \n" +
+                    "(2,'afdadfsf','Jane', 'Doe'), \n" +
+                    "(3,'addfddads', 'James', 'Dereck'); \n");
+
+             rs = state.executeQuery("SELECT * FROM Persons");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void removeRecordsTest() {
+            BirthCheck to = new BirthCheck();
+            assertThrows(SQLDataException.class, () -> to.removeRecords(rs));
+    }
+}
