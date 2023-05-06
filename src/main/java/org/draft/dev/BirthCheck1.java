387dee0c398109316c61e293e881622968c16c96
MrNineMan <108700682+MrNineMan@users.noreply.github.com>
2023-05-06 10:39:33 +0200

    BirthCheck1

---
+++ src/main/java/org/draft/dev/BirthCheck1.java
@@ -0,0 +1,30 @@
+package org.draft.dev;
+
+//Add the necessary SQL imports 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class BirthCheck {
+	//Recieves a ResultSet   
+    void removeRecords(ResultSet rs) {
+        try {
+            //Loops through the result set
+            while (rs.next()) {
+                // Retrieves the birtdate from the second column
+                java.sql.Date birthDate = rs.getDate(2);              
+                //Checks if the birthyear is lesser 1990 
+                if (1990 > birthDate.getYear()) {
+                    //if the above condition is true, delete the record
+                    rs.deleteRow();
+                }
+
+            }
+		
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
